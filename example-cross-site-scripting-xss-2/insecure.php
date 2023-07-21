307bbe12a0e64aa16515ab9e52537b5830c434e4
maucaruso <carusojr@outlook.com>
2023-07-20 21:10:50 -0300

    chore: finishing the xss 2 example

--- example-cross-site-scripting-xss-2/insecure.php
+++ example-cross-site-scripting-xss-2/insecure.php
@@ -1,18 +1,53 @@
-<!-- TODO: Register or updade an user with a xss and then show the user details in the screen without treatative -->
+<!-- GET: http://localhost:8080/example-cross-site-scripting-xss-2/insecure.php?id=2 -->
+
+<?php
+  if(isset($_POST['user_name']) && isset($_POST['email']) && isset($_GET["id"])) {
+    $db = new PDO('sqlite:./../db/db.sqlite');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    $sql = "UPDATE users SET user_name = :user_name, email = :email WHERE user_id = :id";
+    
+    $stmt = $db->prepare($sql);
+    
+    $stmt->bindValue(':id', $_GET["id"], PDO::PARAM_INT);
+    $stmt->bindValue(':user_name', $_POST['user_name'], PDO::PARAM_STR);
+    $stmt->bindValue(':email', $_POST['email'], PDO::PARAM_STR);
+    
+    $stmt->execute();
+  }
+?>
+
+<?php
+  $id = (isset($_GET["id"])) ? $_GET["id"] : 1; 
+
+  $db = new PDO('sqlite:./../db/db.sqlite');
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+  $sql = "SELECT * FROM users WHERE user_id = :id";
+  
+  $stmt = $db->prepare($sql);
+  $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+  $stmt->execute();
+  
+  $user = [];
+  
+  while ($resultado = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $user = $resultado;
+  }
+?>
+
+<h2>Atualizar Usuário</h2>
 
 <form method="post">
 
-  <input type="text" name="busca">
-  <button type="submit">Enviar</button>
+  <input type="text" name="user_name" placeholder="Nome de usuário" value="<?= $user['user_name']; ?>">
+  <input type="text" name="email" placeholder="Email" value="<?= $user['email']; ?>">
+  
+  <button type="submit">Atualizar Usuário</button>
 
 </form>
 
-<?php
+<h3>Meu Cadastro</h3>
 
-if(isset($_POST['busca'])) {
-
-  echo $_POST['busca'];
-
-}
-
-?>
+<p>Usuário: <?= $user['user_name']; ?></p>
+<p>Email: <?= $user['email']; ?></p>
--- example-cross-site-scripting-xss-2/secure.php
+++ example-cross-site-scripting-xss-2/secure.php
@@ -1,20 +1,56 @@
-<!-- TODO: Register or updade an user with a xss -->
+<!-- GET: http://localhost:8080/example-cross-site-scripting-xss-2/secure.php?id=2 -->
+
+<?php
+  if(isset($_POST['user_name']) && isset($_POST['email']) && isset($_GET["id"])) {
+    $userName = strip_tags($_POST['user_name']);
+    $email = strip_tags($_POST['email']);
+    
+    $db = new PDO('sqlite:./../db/db.sqlite');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    $sql = "UPDATE users SET user_name = :user_name, email = :email WHERE user_id = :id";
+    
+    $stmt = $db->prepare($sql);
+    
+    $stmt->bindValue(':id', $_GET["id"], PDO::PARAM_INT);
+    $stmt->bindValue(':user_name', $userName, PDO::PARAM_STR);
+    $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+    
+    $stmt->execute();
+  }
+?>
+
+<?php
+  $id = (isset($_GET["id"])) ? $_GET["id"] : 1; 
+
+  $db = new PDO('sqlite:./../db/db.sqlite');
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+  $sql = "SELECT * FROM users WHERE user_id = :id";
+  
+  $stmt = $db->prepare($sql);
+  $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+  $stmt->execute();
+  
+  $user = [];
+  
+  while ($resultado = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $user = $resultado;
+  }
+?>
+
+<h2>Atualizar Usuário</h2>
 
 <form method="post">
 
-  <input type="text" name="busca">
-  <button type="submit">Enviar</button>
+  <input type="text" name="user_name" placeholder="Nome de usuário" value="<?= strip_tags($user['user_name']); ?>">
+  <input type="text" name="email" placeholder="Email" value="<?= strip_tags($user['email']); ?>">
+  
+  <button type="submit">Atualizar Usuário</button>
 
 </form>
 
-<?php
+<h3>Meu Cadastro</h3>
 
-if(isset($_POST['busca'])) {
-
-  echo strip_tags($_POST['busca']); // removendo todo html e deixando apenas a string
-	echo strip_tags($_POST['busca'], "<strong><a>"); // removendo todos os elementos que não sejam strong ou a do campo
-  echo htmlentities($_POST['busca']); // convertendo html em string
-
-}
-
-?>
+<p>Usuário: <?= strip_tags($user['user_name']); ?></p>
+<p>Email: <?= strip_tags($user['email']); ?></p>
